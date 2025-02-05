c21795986cab82b798ab0fc3f7166a391e813960
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2025-02-05 07:38:31 -0500

    Update dockerFile

--- Week1-Containerization_and_IaaC/pipeline.py
+++ Week1-Containerization_and_IaaC/pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
-df = pd.read_parquet("/Users/tahir/Desktop/Github/dataengineering/Week1-Containerization_and_IaaC/yellow_tripdata_2024-01.parquet")
+df = pd.read_csv("wine_data.csv")
 print(df.head)
 print("---------------------")
 
 # GEt schema
-print(pd.io.sql.get_schema(df, name="yellow_taxi_data"))
+print(df.info())
