40d713a8a9938f51ae27de3ec35397a8f12fe6f2
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2025-04-18 21:10:58 -0400

    Messing around with docker

--- Week1-Containerization_and_IaaC/pipeline.py
+++ Week1-Containerization_and_IaaC/pipeline.py
@@ -2,6 +2,9 @@ import pandas as pd
 df = pd.read_csv("wine_data.csv")
 print(df.head)
 print("---------------------")
-
+for i in range(10):
+    time.sleep(10)
+    print("I AM STILL ALIVE!")
+    print(df.head)
 # GEt schema
 print(df.info())
